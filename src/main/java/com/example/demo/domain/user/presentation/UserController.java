ce83e5440b98ad99b92f91af2f76991d1082c5a4
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-09 21:14:01 +0900

    리펙토링

--- src/main/java/com/example/demo/domain/user/presentation/UserController.java
+++ src/main/java/com/example/demo/domain/user/presentation/UserController.java
@@ -2,7 +2,6 @@ package com.example.demo.domain.user.presentation;
 
 import com.example.demo.domain.user.presentation.dto.request.SignupRequest;
 import com.example.demo.domain.user.service.SignupService;
-import com.example.demo.infra.mail.Service.ResendCodeRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,17 +11,9 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final SignupService signupService;
-    private final ResendCodeRequestService resendCodeRequestService;
 
     @PostMapping
     public void signup(@RequestBody SignupRequest request) {
         signupService.signup(request);
     }
-
-    @PostMapping("code/Resend")
-    public void resendCode(String email) {
-        resendCodeRequestService.ResendCode(email);
-    }
-
-    @PostMapping("/email")
 }
