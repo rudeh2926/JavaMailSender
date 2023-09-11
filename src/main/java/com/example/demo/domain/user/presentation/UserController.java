629554d2b70c996664ce1774c366ae75fd773667
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-11 23:14:01 +0900

    컨트롤러 개발

--- src/main/java/com/example/demo/domain/user/presentation/UserController.java
+++ src/main/java/com/example/demo/domain/user/presentation/UserController.java
@@ -1,8 +1,11 @@
 package com.example.demo.domain.user.presentation;
 
-import com.example.demo.domain.user.presentation.dto.request.UserSignupRequest;
-import com.example.demo.domain.user.service.UserSignupService;
+import com.example.demo.domain.user.presentation.dto.request.*;
+import com.example.demo.domain.user.presentation.dto.response.QueryDetaileMyInfoResponse;
+import com.example.demo.domain.user.presentation.dto.response.QueryMyInfoResponse;
+import com.example.demo.domain.user.service.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,9 +14,35 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserSignupService userSignupService;
+    private final ModifyNicknameAndPasswordService modifyNicknameAndPasswordService;
+    private final ModifyEmailService modifyEmailService;
+    private final QueryDetaileMyInfoService queryDetaileMyInfoService;
+    private final QueryMyInfoService queryMyInfoService;
 
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void signup(@RequestBody UserSignupRequest request) {
         userSignupService.signup(request);
+    }
+
+    @PatchMapping("/modify")
+    public void modifyNicknameAndPassword(@RequestBody ModifyNicknameAndPasswordRequest modifyNicknameAndPasswordRequest) {
+        modifyNicknameAndPasswordService.modifyNicknameAndPassword(modifyNicknameAndPasswordRequest);
+    }
+
+    @PatchMapping("/email/modify")
+    public void modifyEmail(@RequestBody ModifyEmailRequest modifyEmailRequest) {
+        modifyEmailService.modifyEmail(modifyEmailRequest);
+    }
+
+    @GetMapping("/detaile/info")
+    public QueryDetaileMyInfoResponse queryDetaileMyInfo(@RequestParam String password) {
+        return queryDetaileMyInfoService.QueryDetaileInfo(password);
+    }
+
+    @GetMapping("/info")
+    public QueryMyInfoResponse queryMyInfo() {
+       return queryMyInfoService.queryMyInfo();
     }
 }
